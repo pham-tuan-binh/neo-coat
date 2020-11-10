b1d379de1a6aefe19c881a313c98837d24eed056
pham-tuan-binh <phamtuanbinh1504@gmail.com>
2020-11-10 12:30:29 +0700

    NeoCoat with snake game

--- Neopixel controller/lib/snake/Snake.cpp
+++ Neopixel controller/lib/snake/Snake.cpp
@@ -1,60 +1,40 @@
-/*
-	Yet Another Snake Game Library.
-	Copyright (C) 2014  Emanuel Knöpfel
-
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 #include "Snake.h"
 
-
-Snake::Snake(int fieldSizeX,int fieldSizeY, int moveDelay)
+Snake::Snake(int fieldSizeX, int fieldSizeY, int moveDelay)
 {
 	randomSeed(analogRead(0));
 	_fieldSizeX = fieldSizeX;
 	_fieldSizeY = fieldSizeY;
 
-	food=new pixel[1];
+	food = new pixel[1];
 
-	snakeLimbs=new pixel[_fieldSizeX*_fieldSizeY];
-	_snakeLength=1;
+	snakeLimbs = new pixel[_fieldSizeX * _fieldSizeY];
+	_snakeLength = 1;
 	snakeLimbs[0].posX = _fieldSizeX / 2;
 	snakeLimbs[0].posY = _fieldSizeY / 2;
 
-	_bodyColor.set(0,255,0);
-	_headColor.set(0,240,60);
-	_foodColor.set(255,0,0);
+	_bodyColor.set(0, 255, 0);
+	_headColor.set(0, 240, 60);
+	_foodColor.set(255, 0, 0);
 	setColor(_bodyColor, 1);
 	setColor(_headColor, 2);
 	setColor(_foodColor, 3);
 
 	relocateFood();
 
-
 	_direction = RIGHT;
-	_wasReset=false;
+	_wasReset = false;
 	_moveDelay = moveDelay;
-	_snakeTimer =_moveDelay;
+	_snakeTimer = _moveDelay;
 }
-
 
 bool Snake::checkMove(pixel newPos)
 {
-	if(isPointOnSnake(newPos))
+	if (isPointOnSnake(newPos))
 		return false;
-	if(newPos.posX<0 || newPos.posX>=_fieldSizeX)
+	if (newPos.posX < 0 || newPos.posX >= _fieldSizeX)
 		return false;
-	if(newPos.posY<0 || newPos.posY>=_fieldSizeY)
+	if (newPos.posY < 0 || newPos.posY >= _fieldSizeY)
 		return false;
 
 	return true;
@@ -62,65 +42,60 @@ bool Snake::checkMove(pixel newPos)
 
 void Snake::resetGame()
 {
-	_snakeLength=1;
+	_snakeLength = 1;
 	snakeLimbs[0].posX = _fieldSizeX / 2;
 	snakeLimbs[0].posY = _fieldSizeY / 2;
-	_wasReset=true;
+	_wasReset = true;
 }
-
 
 void Snake::makeMove(byte direction)
 {
 
-	for(int i=_snakeLength-1;i>=0; i--)
+	for (int i = _snakeLength - 1; i >= 0; i--)
 	{
-		if (i==0)
+		if (i == 0)
 		{
 
 			pixel newPos = snakeLimbs[0];
-			switch(direction)
+			switch (direction)
 			{
-					case LEFT: 
-							newPos.posX--;
-						break;
-					case RIGHT:
-							newPos.posX++;
-						break;
-					case UP:
-							newPos.posY++;
-						break;
-					case DOWN:
-							newPos.posY--;
-						break;
+			case LEFT:
+				newPos.posX--;
+				break;
+			case RIGHT:
+				newPos.posX++;
+				break;
+			case UP:
+				newPos.posY++;
+				break;
+			case DOWN:
+				newPos.posY--;
+				break;
 			}
 
-
-			if(checkMove(newPos))
+			if (checkMove(newPos))
 			{
-				snakeLimbs[0]=newPos;
+				snakeLimbs[0] = newPos;
 			}
 			else
 				resetGame();
 		}
 		else
 		{
-			snakeLimbs[i].posX = snakeLimbs[i-1].posX;
-			snakeLimbs[i].posY = snakeLimbs[i-1].posY;
+			snakeLimbs[i].posX = snakeLimbs[i - 1].posX;
+			snakeLimbs[i].posY = snakeLimbs[i - 1].posY;
 		}
 	}
-	
 }
 
-
-
 void Snake::tick()
-{	
+{
 	_snakeTimer--;
-	if(_snakeTimer==0)
+	if (_snakeTimer == 0)
 	{
 		makeMove(_direction);
 		checkFood();
-		_snakeTimer=_moveDelay;
+		_snakeTimer = _moveDelay;
 	}
 }
 
@@ -149,27 +124,27 @@ int Snake::getSnakeLenght()
 	return _snakeLength;
 }
 
-Snake::pixel* Snake::getSnakeLimbs()
+Snake::pixel *Snake::getSnakeLimbs()
 {
 	return snakeLimbs;
 }
 
 void Snake::increaseSize()
 {
-	if(_snakeLength<_fieldSizeX*_fieldSizeY)
+	if (_snakeLength < _fieldSizeX * _fieldSizeY)
 	{
 		_snakeLength++;
-		snakeLimbs[_snakeLength-1].posX=snakeLimbs[_snakeLength-2].posX;
-		snakeLimbs[_snakeLength-1].posY=snakeLimbs[_snakeLength-2].posY;
-		snakeLimbs[_snakeLength-1].pixelColor=_bodyColor;
+		snakeLimbs[_snakeLength - 1].posX = snakeLimbs[_snakeLength - 2].posX;
+		snakeLimbs[_snakeLength - 1].posY = snakeLimbs[_snakeLength - 2].posY;
+		snakeLimbs[_snakeLength - 1].pixelColor = _bodyColor;
 	}
 }
 
 bool Snake::wasGameReset()
 {
-	if(_wasReset)
+	if (_wasReset)
 	{
-		_wasReset=false;
+		_wasReset = false;
 		return true;
 	}
 	else
@@ -180,15 +155,14 @@ bool Snake::wasGameReset()
 
 void Snake::checkFood()
 {
-	if(snakeLimbs[0].posX==food[0].posX && snakeLimbs[0].posY==food[0].posY)
+	if (snakeLimbs[0].posX == food[0].posX && snakeLimbs[0].posY == food[0].posY)
 	{
 		increaseSize();
 		relocateFood();
 	}
-
 }
 
-Snake::pixel* Snake::getFoodPositions()
+Snake::pixel *Snake::getFoodPositions()
 {
 	return food;
 }
@@ -197,16 +171,16 @@ void Snake::relocateFood()
 {
 	do
 	{
-		food[0].posX = random(0, 14);
-		food[0].posY = random(0, 8);
-	}while(isPointOnSnake(food[0]));
+		food[0].posX = random(0, _fieldSizeX);
+		food[0].posY = random(0, _fieldSizeY);
+	} while (isPointOnSnake(food[0]));
 }
 
 bool Snake::isPointOnSnake(pixel pointPos)
 {
-	for(int i=0; i<_snakeLength; i++)
+	for (int i = 0; i < _snakeLength; i++)
 	{
-		if(pointPos.posX==snakeLimbs[i].posX && pointPos.posY==snakeLimbs[i].posY)
+		if (pointPos.posX == snakeLimbs[i].posX && pointPos.posY == snakeLimbs[i].posY)
 		{
 			return true;
 		}
@@ -216,11 +190,11 @@ bool Snake::isPointOnSnake(pixel pointPos)
 
 void Snake::setColor(colorRGB color, byte destination)
 {
-	switch(destination)
+	switch (destination)
 	{
 	case 1:
 		_bodyColor = color;
-		for(int i=1;i<_snakeLength;i++)
+		for (int i = 1; i < _snakeLength; i++)
 		{
 			snakeLimbs[i].pixelColor = _bodyColor;
 		}
--- Neopixel controller/src/main.cpp
+++ Neopixel controller/src/main.cpp
@@ -7,12 +7,15 @@
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 
+// Custom library
+#include <Snake.h> // Original by Emanuel Knöpfel (This library have been modified for compatibility. Original lib may malfunction in this code)
+
 using namespace std;
 
 #define WIDTH 6
 #define HEIGHT 6
-#define SSID "NeoCoat - MK1"
-#define PASSWORD "123456789"
+#define SSID "NeoCoat (^˵◕ω◕˵^)"
+#define PASSWORD "0903470077"
 
 const unsigned int length = WIDTH * HEIGHT;
 
@@ -22,8 +25,13 @@ IPAddress apIP(42, 42, 42, 42);
 
 bool state = false;
 
+bool snakeState = false;
+
 // AsyncWebServer on port 80
 AsyncWebServer server(80);
+
+// Snake game
+Snake snakeGame(6, 6, 10);
 
 // Color convert functions
 long stringToHex(string color)
@@ -34,7 +42,8 @@ long stringToHex(string color)
 }
 
 // Draw functions
-void drawPixel(int x, int y, string color)
+// Hex string
+byte drawPixel(int x, int y, string color)
 {
   if (x > 0 && x <= WIDTH && y > 0 && y <= HEIGHT)
   {
@@ -56,12 +65,89 @@ void drawPixel(int x, int y, string color)
 
     FastLED.show();
   }
+  return 0;
+}
+// RGB value for snake library
+byte drawPixel(byte x, byte y, byte r, byte g, byte b)
+{
+  if (x > 0 && x <= WIDTH && y > 0 && y <= HEIGHT)
+  {
+    byte position;
+
+    // Because our matrix data flow is opposite for each concurrent line. We need to change x position based on the line's number.
+    // If your matrix is parralel, you must change this part of code.
+    // !!!
+    if (y % 2 == 0)
+    {
+      position = y * WIDTH - (x - 1);
+    }
+    else
+    {
+      position = (y - 1) * WIDTH + x;
+    }
+
+    leds[position - 1] = CRGB(r, g, b);
+
+    FastLED.show();
+
+    return position;
+  }
+  return 0;
 }
 
+// Functions to adapt snake game. Might overlap existing functions.
+void clearScreen()
+{
+  fill_solid(leds, length, CRGB(3, 3, 3));
+  FastLED.show();
+}
+
+void changeRGBtoGBR()
+{
+  for (unsigned int whiteLed = 0; whiteLed < length; whiteLed++)
+  {
+    leds[whiteLed].setRGB(leds[whiteLed].g, leds[whiteLed].b, leds[whiteLed].r);
+  }
+}
+
+void snakeLoop()
+{
+  Snake::pixel *snakeLimbs = snakeGame.getSnakeLimbs();   //this needs to be updated every frame
+  Snake::pixel *snakeFood = snakeGame.getFoodPositions(); //this needs to be updated every frame
+  clearScreen();
+  drawPixel(snakeFood[0].posX + 1, HEIGHT - snakeFood[0].posY, snakeFood[0].pixelColor.r, snakeFood[0].pixelColor.g, snakeFood[0].pixelColor.b); // display the food
+  for (int i = 0; i < snakeGame.getSnakeLenght(); i++)
+  {
+    //display the snake, my setpixel method has x=0, y=0 at the top left, but the library has it at bottom left, so I invert the Y-Axis:
+    drawPixel(snakeLimbs[i].posX + 1, HEIGHT - snakeLimbs[i].posY, snakeLimbs[i].pixelColor.r, snakeLimbs[i].pixelColor.g, snakeLimbs[i].pixelColor.b);
+  }
+  FastLED.show();
+  snakeGame.tick();             //main loop for the snake library
+  if (snakeGame.wasGameReset()) // if the snake bit itself or the wall, flash a little
+  {
+    for (int i = 0; i < 30; i++)
+    {
+      changeRGBtoGBR();
+      FastLED.show();
+      delay(40);
+    }
+    snakeState = false;
+  }
+  else
+    delay(30);
+}
+
+// Set up
 void setup()
 {
   // Begin serial
   Serial.begin(9600);
+
+  // Set up fast leds;
+  FastLED.addLeds<NEOPIXEL, 1>(leds, length);
+
+  FastLED.clear();
+  FastLED.show();
 
   // Set up wifi access point
   WiFi.mode(WIFI_AP_STA);
@@ -87,8 +173,10 @@ void setup()
     Serial.println("Mount filesystem successfully");
   }
 
-  // Set up fast leds;
-  FastLED.addLeds<NEOPIXEL, 1>(leds, length);
+  // Set up snake
+  snakeGame.setBodyColor(255, 0, 255); //optionally set the color of the snakeparts
+  snakeGame.setFoodColor(0, 60, 125);  //optionally set the color of the food
+  snakeGame.setHeadColor(225, 20, 60); //optionally set the color of the snakeparts
 
   // Set up server
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
@@ -138,11 +226,64 @@ void setup()
   });
 
   server.on("/snake", HTTP_GET, [](AsyncWebServerRequest *request) {
+    snakeState = true;
+
     request->send(200, "text/plain", "Okela");
+  });
+
+  server.on("/command", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Okela");
+
+    if (request->hasParam("id"))
+    {
+      AsyncWebParameter *id_pointer = request->getParam("id");
+      int id = atoi(id_pointer->value().c_str());
+
+      Serial.println("Movement:");
+      Serial.print(id);
+
+      switch (id)
+      {
+      case 0:
+        snakeState = true;
+        break;
+      case 1:
+        snakeGame.goUp();
+        break;
+      case 2:
+        snakeGame.goRight();
+        break;
+      case 3:
+        snakeGame.goDown();
+        break;
+      case 4:
+        snakeGame.goLeft();
+        break;
+      case 5:
+        snakeState = false;
+        fill_solid(leds, length, CRGB::Gray);
+        FastLED.show();
+        break;
+      default:
+        break;
+      }
+    }
+    else
+    {
+      request->send(400, "text/plain", "Missing id");
+    }
   });
 
   server.begin();
 }
 void loop()
 {
+  if (state && snakeState)
+  {
+    snakeLoop();
+    Serial.println("Running");
+    delay(40);
+  }
 }
+
+// Code written by Pham Tuan Binh. Feel free to modify it, just credit me in the end.
